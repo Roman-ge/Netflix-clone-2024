d71574969ecfbedfd56c68d8db6bd604941a8de6
Roman-ge <rgeleta1@gmail.com>
2024-11-21 11:47:41 -0600

    Update Footer.jsx

--- Netflix-clone/src/Components/Footer/Footer.jsx
+++ Netflix-clone/src/Components/Footer/Footer.jsx
@@ -52,43 +52,7 @@ const Footer = () => {
       </div>
     </div>
 
-    // <footer className="netflix-footer">
-    //   <div className="container">
-    //     {/* Footer Links */}
-    //     <div className="footer-links">
-    //       <ul>
-    //         <li><a href="#">FAQ</a></li>
-    //         <li><a href="#">Help Center</a></li>
-    //         <li><a href="#">Account</a></li>
-    //         <li><a href="#">Media Center</a></li>
-    //         <li><a href="#">Investor Relations</a></li>
-    //         <li><a href="#">Jobs</a></li>
-    //         <li><a href="#">Ways to Watch</a></li>
-    //         <li><a href="#">Terms of Use</a></li>
-    //         <li><a href="#">Privacy</a></li>
-    //         <li><a href="#">Cookie Preferences</a></li>
-    //         <li><a href="#">Corporate Information</a></li>
-    //         <li><a href="#">Contact Us</a></li>
-    //       </ul>
-    //     </div>
-
-    //     {/* Social Links */}
-    //     <div className="footer-social">
-    //       <p>Follow us on:</p>
-    //       <ul>
-    //         <li><FacebookIcon /></li>
-    //         <li><InstagramIcon /></li>
-    //         <li><YouTubeIcon  /></li>
-
-    //       </ul>
-    //     </div>
-
-    //     {/* Copyright Info */}
-    //     <div className="footer-copyright">
-    //       <p>&copy; {new Date().getFullYear()} Netflix, Inc.</p>
-    //     </div>
-    //   </div>
-    // </footer>
+    
   );
 };
 
