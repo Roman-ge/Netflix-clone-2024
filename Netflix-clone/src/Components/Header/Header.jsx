f8e1726a0646e9df29a5af93199530987b927741
Roman-ge <rgeleta1@gmail.com>
2024-11-22 15:21:25 -0600

    Update Header.jsx

--- Netflix-clone/src/Components/Header/Header.jsx
+++ Netflix-clone/src/Components/Header/Header.jsx
@@ -1,39 +1,63 @@
-import React from 'react'
-import './Header.css'
-import NetflixLogo from '../../assets/images/NetflixLogo.jpg'
-import SearchIcon from '@mui/icons-material/Search';
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-
+import "./header.css";
+import NetflixLogo from "../../assets/Netflix.jpg";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 const Header = () => {
   return (
-    <div className='header_outer_container'>
-      <div className='header_container'>
-        <div className='header_left'>
+    <div className="header_outer_container">
+      <div className="header_container">
+        <div className="header_left">
           <ul>
-            <li><img src={NetflixLogo} alt='Netflix Logo' width='100' /></li>
-            <li>Netflix</li>
+            <li>
+              <img src={NetflixLogo} alt="Netflix Logo " width="100" />
+            </li>
             <li>Home</li>
-            <li>TVShow</li>
+            <li>TvShows</li>
             <li>Movies</li>
             <li>Latest</li>
             <li>MyList</li>
-            <li>Browse by language</li>
-            
+            <li>Browse by Languges</li>
           </ul>
         </div>
-        <div className='header_right'>
+        <div className="header_right">
           <ul>
-            <li><SearchIcon /></li>
-            <li><NotificationsNoneIcon /></li>
-            <li><AccountBoxIcon /></li>
-            <li><ArrowDropDownIcon /></li>
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsNoneIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              {" "}
+              <ArrowDropDownIcon />
+            </li>
           </ul>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
